d3314d9e30877d344601d52b075c941bacbffb41
Jannis Göing <jannis.goeing@pm.me>
2025-11-19 15:41:28 +0100

    rework with setup from scaffold

---
+++ custom_components/iternio/const.py
@@ -0,0 +1,8 @@
+"""Constants for the A Better Route Planner integration."""
+
+DOMAIN = "iternio"
+
+# TODO Update with your own urls
+OAUTH2_AUTHORIZE = "https://abetterrouteplanner.com/oauth/auth"
+OAUTH2_TOKEN = "https://api.iternio.com/1/oauth/token"
+BASE_URL = "https://api.iternio.com/1"
